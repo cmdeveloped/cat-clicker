7028766c86e24550fac1d300a03ec125b7d839df
cmdeveloped <cmwebdevs@gmail.com>
2017-06-29 00:29:39 -0500

    Update js with app model.

---
+++ js/app.js
@@ -0,0 +1,35 @@
+var model = {
+  currentCat: null,
+  cats: [
+    {
+      name:'Bluesy',
+      id:'bluesy',
+      img:'img/bluesy.jpg',
+      clickCount: 0,
+    },
+    {
+      name:'Baby Blue',
+      id:'babyblue',
+      img:'img/babyblue.jpg',
+      clickCount: 0,
+    },
+    {
+      name:'Huggers',
+      id:'huggers',
+      img:'img/huggers.jpg',
+      clickCount: 0,
+    },
+    {
+      name:'Tiny',
+      id:'tiny',
+      img:'img/tiny.jpg',
+      clickCount: 0,
+    },
+    {
+      name:'Gingy',
+      id:'gingy',
+      img:'img/gingy.jpg',
+      clickCount: 0,
+    }
+  ]
+};
